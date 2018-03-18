a99cc4fa5a1d42b25a5649d24192ef269068acd5
kkalugerov <k.kalugerov@yatrusanalytics.com>
2018-03-18 15:06:02 +0200

    Replace string publisher in Book entity with Publisher Entity

--- src/main/java/zealot/springframework/springwebapp/bootstrap/DevBootstrap.java
+++ src/main/java/zealot/springframework/springwebapp/bootstrap/DevBootstrap.java
@@ -5,8 +5,10 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 import zealot.springframework.springwebapp.models.Author;
 import zealot.springframework.springwebapp.models.Book;
+import zealot.springframework.springwebapp.models.Publisher;
 import zealot.springframework.springwebapp.repository.AuthorRepository;
 import zealot.springframework.springwebapp.repository.BookRepository;
+import zealot.springframework.springwebapp.repository.PublisherRepository;
 
 
 @Component
@@ -17,10 +19,14 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private BookRepository bookRepository;
 
+    private PublisherRepository publisherRepository;
+
     //Dependency injection
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository,
+                        PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -29,16 +35,30 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
     }
 
     private void initData() {
+
+        Publisher publisher1 = new Publisher();
+        publisher1.setName("publisher 1");
+        publisher1.setAddress("address1");
+
+        publisherRepository.save(publisher1);
+
         Author ivan = new Author("Ivan", "Bogrov");
-        Book book1 = new Book("This is test book ", "1234", "Agency1");
+        Book book1 = new Book("This is test book ", "1234", publisher1);
         ivan.getBooks().add(book1);
         book1.getAuthors().add(ivan);
 
         authorRepository.save(ivan);
         bookRepository.save(book1);
 
+
+        Publisher publisher2 = new Publisher();
+        publisher2.setName("publisher 2");
+        publisher2.setAddress("address2");
+
+        publisherRepository.save(publisher2);
+
         Author asen = new Author("Asen", "Asenov");
-        Book book2 = new Book("This is test book2 ", "4321", "Agency2");
+        Book book2 = new Book("This is test book2 ", "4321", publisher2);
         asen.getBooks().add(book2);
 
         authorRepository.save(asen);

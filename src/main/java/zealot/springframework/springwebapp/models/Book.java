29d6351b3367f532573c41eccdd5d2a7f236e9b8
kkalugerov <k.kalugerov@yatrusanalytics.com>
2018-03-18 13:42:49 +0200

    First commit of SB project

---
+++ src/main/java/zealot/springframework/springwebapp/models/Book.java
@@ -0,0 +1,105 @@
+package zealot.springframework.springwebapp.models;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+
+@Entity
+public class Book {
+
+    //JPA - java persistence API
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) // JPA automatic generate id for us
+    private Long id;
+    private String title;
+    private String ibsn;
+    private String publisher;
+
+    @ManyToMany // JPA generate many to many relationship
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+    inverseJoinColumns = @JoinColumn(name = "author_id "))
+    private Set<Author> authors = new HashSet<>();
+
+    public Book(){};
+
+    public Book(String title, String ibsn, String publisher) {
+        this.title = title;
+        this.ibsn = ibsn;
+        this.publisher = publisher;
+    }
+
+    public Book(String title, String ibsn, String publisher, Set<Author> authors) {
+        this.title = title;
+        this.ibsn = ibsn;
+        this.publisher = publisher;
+        this.authors = authors;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIbsn() {
+        return ibsn;
+    }
+
+    public void setIbsn(String ibsn) {
+        this.ibsn = ibsn;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", ibsn='" + ibsn + '\'' +
+                ", publisher='" + publisher + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+}
